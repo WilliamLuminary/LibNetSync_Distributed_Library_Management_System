188e8f16c56b4ebf8c47f87b0dc471719b7c5e00
William <93109493+WilliamLuminary@users.noreply.github.com>
2023-11-13 03:09:08 -0800

    Restructure codes

--- serverM.cpp
+++ serverM.cpp
@@ -3,8 +3,7 @@
 //
 
 #include <cstdio>
-#include <sstream>  // Include this at the top of your file
-
+#include <sstream>
 #include <cstdlib>
 #include <unistd.h>
 #include <cerrno>
@@ -41,73 +40,197 @@ const int SERVER_M_TCP_PORT = SERVER_M_TCP_BASE_PORT + LAST_3_DIGITS_YAXING_LI_U
 const int LISTEN_BACK_LOG = 128;
 const int BUFFER_SIZE = 1024;
 
-bool starts_with(const std::string &fullString, const std::string &prefix);
+int initialize_udp_server(int port);
 
-int tcp_kernel();
+void receive_initial_book_statuses(int sfd, unordered_map<string, int> &bookStatuses);
 
-int udp_kernel();
+void handle_commands(int sfd);
+
+void send_acknowledgment(int sfd, const sockaddr_in &addr, const string &message);
 
 unordered_map<string, int> deserialize_book_statuses(const string &data);
 
+int initialize_tcp_server(int port);
+
+int handle_new_connection(int sfd);
+
+void communicate_with_client(int cfd);
+
 int main() {
-    udp_kernel();
-    tcp_kernel();
+    int udp_sfd = initialize_udp_server(SERVER_M_UDP_PORT);
+    if (udp_sfd < 0) {
+        return 1;
+    }
+
+    unordered_map<string, int> combinedBookStatuses;
+    receive_initial_book_statuses(udp_sfd, combinedBookStatuses);
+    handle_commands(udp_sfd);
+    close(udp_sfd); // Close the UDP socket
+
+    int tcp_sfd = initialize_tcp_server(SERVER_M_TCP_PORT);
+    if (tcp_sfd < 0) {
+        return 1;
+    }
+
+    int client_fd = handle_new_connection(tcp_sfd);
+    if (client_fd < 0) {
+        close(tcp_sfd);
+        return 1;
+    }
+
+    communicate_with_client(client_fd);
+
+    close(client_fd);
+    close(tcp_sfd); // Close the TCP socket
     return 0;
 }
 
-int tcp_kernel() {
-    // 1. Initial TCP socket file descriptor for the server
+
+int initialize_udp_server(int port) {
+    int sfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (sfd == -1) {
+        cerr << "Initial UDP Socket failed: " << strerror(errno) << endl;
+        return -1;
+    }
+
+    struct sockaddr_in sock_addr_in{};
+    sock_addr_in.sin_family = AF_INET;
+    sock_addr_in.sin_port = htons(port);
+    sock_addr_in.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(sfd, (struct sockaddr *) &sock_addr_in, sizeof(sock_addr_in)) == -1) {
+        cerr << "Bind UDP socket failed: " << strerror(errno) << endl;
+        close(sfd);
+        return -1;
+    }
+
+    return sfd;
+}
+
+void receive_initial_book_statuses(int sfd, unordered_map<string, int> &bookStatuses) {
+    bool receivedS = false, receivedL = false, receivedH = false;
+    char buffer[BUFFER_SIZE];
+    struct sockaddr_in sender_addr{};
+    socklen_t sender_addr_len = sizeof(sender_addr);
+
+    while (!(receivedS && receivedL && receivedH)) {
+        ssize_t len = recvfrom(sfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *) &sender_addr, &sender_addr_len);
+        if (len > 0) {
+            string receivedData(buffer, len);
+            auto tempStatuses = deserialize_book_statuses(receivedData);
+
+            for (const auto &entry: tempStatuses) {
+                bookStatuses[entry.first] = entry.second; // Insert into the main map
+                if (entry.first[0] == 'S') receivedS = true;
+                else if (entry.first[0] == 'L') receivedL = true;
+                else if (entry.first[0] == 'H') receivedH = true;
+            }
+
+            send_acknowledgment(sfd, sender_addr, "Acknowledged");
+        } else if (len < 0) {
+            cerr << "Error receiving data: " << strerror(errno) << endl;
+            break;
+        }
+    }
+    cout << "Received all initial book statuses." << endl;
+}
+
+void handle_commands(int sfd) {
+    char buffer[BUFFER_SIZE];
+    struct sockaddr_in sender_addr{};
+    socklen_t sender_addr_len = sizeof(sender_addr);
+
+    while (true) {
+        ssize_t len = recvfrom(sfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *) &sender_addr, &sender_addr_len);
+        if (len > 0) {
+            string command(buffer, len);
+            cout << "Received command: " << command << endl;
+            // TODO: Process the command
+            send_acknowledgment(sfd, sender_addr, "Command processed"); // Placeholder response
+        } else if (len < 0) {
+            cerr << "Error receiving command: " << strerror(errno) << endl;
+            break;
+        }
+    }
+}
+
+void send_acknowledgment(int sfd, const sockaddr_in &addr, const string &message) {
+    ssize_t sent_bytes = sendto(sfd, message.c_str(), message.size(), 0, (const struct sockaddr *) &addr, sizeof(addr));
+    if (sent_bytes < 0) {
+        cerr << "Error sending acknowledgment: " << strerror(errno) << endl;
+    }
+}
+
+unordered_map<string, int> deserialize_book_statuses(const string &data) {
+    unordered_map<string, int> bookStatuses;
+    std::stringstream ss(data);
+    string line;
+
+    while (std::getline(ss, line)) {
+        std::stringstream lineStream(line);
+        string bookCode;
+        int status;
+        if (std::getline(lineStream, bookCode, ',')) {
+            lineStream >> status;
+            bookStatuses[bookCode] = status;
+        }
+    }
+
+    return bookStatuses;
+}
+
+int initialize_tcp_server(int port) {
     int sfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (sfd == -1) {
         cerr << "Initial TCP Socket failed: " << strerror(errno) << endl;
-        return 1;
+        return -1;
     }
 
-    // 2. Bind to the local port
     struct sockaddr_in sock_addr_in{};
     sock_addr_in.sin_family = AF_INET;
-    sock_addr_in.sin_port = htons(SERVER_M_TCP_PORT);
-    sock_addr_in.sin_addr.s_addr = INADDR_ANY;  //“INADDR_ANY” is 0, and no need to do any transformation to 0.
-    // Besides, using this, the socket will set to the local IP.
+    sock_addr_in.sin_port = htons(port);
+    sock_addr_in.sin_addr.s_addr = INADDR_ANY;
 
-    int bind_result = bind(sfd, reinterpret_cast<struct sockaddr *>(&sock_addr_in), sizeof(sock_addr_in));
+    int bind_result = bind(sfd, (struct sockaddr *) &sock_addr_in, sizeof(sock_addr_in));
     if (bind_result == -1) {
         cerr << "Bind TCP socket failed: " << strerror(errno) << endl;
         close(sfd);
-        return 1;
+        return -1;
     }
 
-    // 3. Listen for the potential connection
     int listen_result = listen(sfd, LISTEN_BACK_LOG);
     if (listen_result == -1) {
         cerr << "Listen TCP failed: " << strerror(errno) << endl;
         close(sfd);
-        return 1;
+        return -1;
     }
 
-    // 4. Block and wait for connection from clients
+    return sfd;
+}
+
+int handle_new_connection(int sfd) {
     struct sockaddr_in caddr{};
     socklen_t caddr_len = sizeof(caddr);
-    int cfd = accept(sfd, reinterpret_cast<struct sockaddr *>(&caddr), &caddr_len);
+    int cfd = accept(sfd, (struct sockaddr *) &caddr, &caddr_len);
     if (cfd == -1) {
         cerr << "Accept TCP failed: " << strerror(errno) << endl;
-        close(sfd);
-        return 1;
+        return -1;
     }
 
-    // Print the details on the connection
     char ip[INET_ADDRSTRLEN];
-    cout << "(TCP), IP Address of Client: "
-         << inet_ntop(AF_INET, &caddr.sin_addr, ip, sizeof(ip))
+    cout << "(TCP) IP Address of Client: " << inet_ntop(AF_INET, &caddr.sin_addr, ip, sizeof(ip))
          << ", Port Number: " << ntohs(caddr.sin_port) << endl;
 
-    // 5. Communicate
+    return cfd;
+}
+
+void communicate_with_client(int cfd) {
     char buff[BUFFER_SIZE];
     while (true) {
-        ssize_t len = recv(cfd, buff, sizeof(buff), 0);
+        ssize_t len = recv(cfd, buff, BUFFER_SIZE, 0);
         if (len > 0) {
             cout << "(TCP) Data from Client: " << string(buff, len) << endl;
-            send(cfd, buff, len, 0);
+            send(cfd, buff, len, 0); // Echo back the received data
         } else if (len == 0) {
             cout << "(TCP) Client has disconnected" << endl;
             break;
@@ -116,132 +239,4 @@ int tcp_kernel() {
             break;
         }
     }
-
-    // 6. close all fds
-    close(sfd);
-    close(cfd);
-    return 0;
 }
-
-int udp_kernel() {
-    // 1. Initial UDP socket file descriptor for the server
-    int sfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-    if (sfd == -1) {
-        cerr << "Initial UDP Socket failed: " << strerror(errno) << endl;
-        return 1;
-    }
-
-    // 2. Bind to the local port
-    struct sockaddr_in sock_addr_in{};
-    sock_addr_in.sin_family = AF_INET;
-    sock_addr_in.sin_port = htons(SERVER_M_UDP_PORT); // Replace with your actual port number
-    sock_addr_in.sin_addr.s_addr = INADDR_ANY;
-
-    int bind_result = bind(sfd, reinterpret_cast<struct sockaddr *>(&sock_addr_in), sizeof(sock_addr_in));
-    if (bind_result == -1) {
-        cerr << "Bind UDP socket failed: " << strerror(errno) << endl;
-        close(sfd);
-        return 1;
-    }
-
-    unordered_map<string, int> combinedBookStatuses;
-
-    // 3. Communicate
-    bool receivedS = false, receivedL = false, receivedH = false;
-
-    while (!(receivedS && receivedL && receivedH)) {
-        struct sockaddr_in caddr{};
-        socklen_t addr_len = sizeof(caddr);
-        char buffer[BUFFER_SIZE];
-
-        ssize_t len = recvfrom(sfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&caddr, &addr_len);
-        if (len > 0) {
-            std::string receivedData(buffer, len);
-            auto bookStatuses = deserialize_book_statuses(receivedData);
-
-            // Process each received book status
-            for (const auto& entry : bookStatuses) {
-                char serverIdentifier = entry.first[0]; // Assume the first character is the server identifier
-                combinedBookStatuses[entry.first] = entry.second;
-
-                // Check the identifier to set the flags
-                if (serverIdentifier == 'S') receivedS = true;
-                else if (serverIdentifier == 'L') receivedL = true;
-                else if (serverIdentifier == 'H') receivedH = true;
-            }
-
-            // Send an acknowledgment back to the backend server
-            std::string response = "Acknowledged";
-            sendto(sfd, response.c_str(), response.size(), 0, (struct sockaddr *)&caddr, addr_len);
-        } else if (len == -1) {
-            std::cerr << "(UDP) Receiving failed: " << std::strerror(errno) << std::endl;
-            if (errno == EINTR) continue; // Handle interrupted system calls
-            break; // Exit on other errors
-        }
-    }
-
-    // At this point, all book statuses have been received
-    std::cout << "Received all book statuses from servers S, L, and H." << std::endl;
-
-
-
-    // Main Communication Loop: Handle ongoing commands
-    struct sockaddr_in caddr{};
-    socklen_t addr_len = sizeof(caddr);
-    char buffer[BUFFER_SIZE];
-    while (true) {
-        addr_len = recvfrom(sfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *) &caddr, &addr_len);
-        if (addr_len > 0) {
-            std::string command(buffer, addr_len);
-            // Process the command here...
-            std::cout << "(UDP) Command received: " << command << std::endl;
-
-            // Send response for the command
-            std::string commandResponse = "Command Response";
-            sendto(sfd, commandResponse.c_str(), commandResponse.size(), 0, (struct sockaddr *) &caddr, addr_len);
-        } else if (addr_len == -1) {
-            std::cerr << "(UDP) Command receiving failed: " << std::strerror(errno) << std::endl;
-            if (errno == EINTR) continue; // Handle interrupted system calls
-            // Break the loop if there's an error other than an interruption
-            break;
-        } else if (addr_len == 0) {
-            // This block is likely unnecessary for UDP since a zero-length datagram is valid.
-            std::cout << "(UDP) Server has disconnected" << std::endl;
-            break;
-        }
-    }
-
-    // 4. close all fds
-    close(sfd);
-
-    return 0;
-}
-
-
-unordered_map<string, int> deserialize_book_statuses(const string &data) {
-    unordered_map<string, int> bookStatuses;
-    std::stringstream ss(data);
-    string item;
-
-    while (std::getline(ss, item, '\n')) {
-        std::stringstream itemStream(item);
-        string bookCode;
-        int status;
-
-        if (std::getline(itemStream, bookCode, ',')) {
-            itemStream >> status;
-            bookStatuses[bookCode] = status;
-        }
-    }
-
-    return bookStatuses;
-}
-
-bool starts_with(const std::string &fullString, const std::string &prefix) {
-    if (fullString.length() < prefix.length()) {
-        return false;
-    }
-    return fullString.compare(0, prefix.length(), prefix) == 0;
-}
-
-
